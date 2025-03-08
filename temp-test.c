ffe5cd555061dee43e233c631428f0b91849fa2e
Jatin <rexifiedjatin@gmail.com>
2025-03-08 20:43:21 +0530

    update

--- temp-test.c
+++ temp-test.c
@@ -1,26 +1,53 @@
-#include <stdio.h>
-#include <netinet/in.h>
 #include <sys/socket.h>
-#include <arpa/inet.h>
-#include <sys/types.h>
-#include <unistd.h>
-#include <stdlib.h>
+#include <netinet/in.h>
 #include <string.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/sendfile.h> 
 
 
-#define LISTENADDR "0.0.0.0"
-
-int server_init()
-
-int main(int argc,char &argv[])
+int main()
 {
-    int s;
-    char *port;
+    int socket_info = socket(AF_INET,SOCK_STREAM,0);
+    struct sockaddr_in addr;
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(8080);
+    addr.sin_addr.s_addr = INADDR_ANY;
+    
 
-    if(argc < 2)
-    {
-        fprintf(stderr,"Usage : %s <listenning port>\n")
-    }
+    bind(socket_info,(struct sockaddr*)&addr,sizeof(addr));
 
-    s = server_init();
+    listen(socket_info,10);//10 is the max client limit after that new clients willl be rejected
+    
+    while(1)
+    {   int client_fd = accept(socket_info,0,0);
+
+        char buffer[256] = {0};
+        recv(client_fd,buffer,256,0);
+
+    //    GET /file.html ;
+
+        char* f = buffer +5;
+        *strchr(f,' ') = 0;
+        int opened_fd = open(f,O_RDONLY);
+
+        // Send HTTP response header
+        char header[] = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n";
+        send(client_fd, header, strlen(header), 0);
+        
+        off_t file_size = lseek(opened_fd, 0, SEEK_END);
+        lseek(opened_fd, 0, SEEK_SET);  // Reset file position
+        
+        // Send the entire file
+        off_t offset = 0;
+        sendfile(client_fd, opened_fd, &offset, file_size);        
+
+        close(opened_fd);
+        close(client_fd);
+    };
+    close(socket_info);
+
+    return 0;
 }
